0ff61eacad5aa0b50f533ef37cb6e1efa38c3a84
Gustavo Savio <gustavo.savio@outlook.com.br>
2026-06-04 11:50:20 -0300

    fix: transpile react-native-worklets to resolve Hermes private class fields error

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,9 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+// react-native-worklets uses private class fields (#) that Hermes requires transpiled
+config.transformer.transformIgnorePatterns = [
+  'node_modules/(?!(react-native|@react-native|react-native-worklets|react-native-reanimated|expo|@expo|@unimodules|nativewind|react-native-css-interop|react-native-safe-area-context|react-native-screens|react-native-gesture-handler)/)',
+];
+
 module.exports = withNativeWind(config, { input: './global.css' });
