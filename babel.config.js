3062de564fc12a49d67fb8904596eeec595186ca
Gustavo Savio <gustavo.savio@outlook.com.br>
2026-06-04 12:40:22 -0300

    fix: force class-properties + private-methods babel transform for reanimated 4.x

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,11 @@ module.exports = function (api) {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
     ],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      // Force private field transpilation for node_modules (reanimated 4.x / worklets use #field syntax)
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
