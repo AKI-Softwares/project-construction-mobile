abb7e9ef302597b470b39030987331a5a5d1a9b0
Gustavo Savio <gustavo.savio@outlook.com.br>
2026-06-04 11:53:08 -0300

    fix: apply transformIgnorePatterns after withNativeWind

--- metro.config.js
+++ metro.config.js
@@ -3,9 +3,11 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+const nativeWindConfig = withNativeWind(config, { input: './global.css' });
+
 // react-native-worklets uses private class fields (#) that Hermes requires transpiled
-config.transformer.transformIgnorePatterns = [
+nativeWindConfig.transformer.transformIgnorePatterns = [
   'node_modules/(?!(react-native|@react-native|react-native-worklets|react-native-reanimated|expo|@expo|@unimodules|nativewind|react-native-css-interop|react-native-safe-area-context|react-native-screens|react-native-gesture-handler)/)',
 ];
 
-module.exports = withNativeWind(config, { input: './global.css' });
+module.exports = nativeWindConfig;
