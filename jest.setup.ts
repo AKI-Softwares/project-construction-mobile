b19e37ef6c88271f19df2333b33db3439959642f
Gustavo Savio <gustavo.savio@outlook.com.br>
2026-06-08 00:40:04 -0300

    feat(add+fix) implements visit screen/services and fix apk connection

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,3 @@
-import '@testing-library/react-native/extend-expect';
-
 jest.mock('expo-secure-store', () => ({
   getItemAsync: jest.fn(() => Promise.resolve(null)),
   setItemAsync: jest.fn(() => Promise.resolve()),
